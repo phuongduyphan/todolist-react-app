5005931ff33c8f34ec027ff44a4fa9186dae7f3c
Duy Phan <phuongduyphan@gmail.com>
2019-01-04 15:45:33 +0700

    add onItemClicked function

--- src/App.js
+++ src/App.js
@@ -6,21 +6,44 @@ import TodoItem from './components/TodoItem';
 class App extends Component {
   constructor() {
     super();
-    this.todoItems = [
-      { title: 'Go Shopping', isComplete: true },
-      { title: 'Play Football' },
-      { title: 'Have dinner' },
-    ];
+    this.state = {
+      todoItems: [
+        { title: 'Go Shopping' },
+        { title: 'Play Football' },
+        { title: 'Have dinner' },
+      ]
+    }
+  }
+
+  onItemClicked = (item) => (event) => {
+    const { isComplete } = item;
+    const { todoItems } = this.state;
+    const index = todoItems.indexOf(item);
+    this.setState({
+      todoItems: [
+        ...todoItems.slice(0, index),
+        {
+          ...item,
+          isComplete: !isComplete
+        },
+        ...todoItems.slice(index + 1)
+      ]
+    });
   }
 
   render() {
     return (
       <div className="App">
         {
-          this.todoItems.length > 0 && this.todoItems.map((item, index) => <TodoItem key={index} item={item} />)
+          this.state.todoItems.length > 0 && this.state.todoItems.map((item, index) => 
+            <TodoItem
+              key={index} 
+              item={item} 
+              onItemClicked={this.onItemClicked(item)} />
+          )
         }
         {
-          this.todoItems.length === 0 && 'Nothing here.'
+          this.state.todoItems.length === 0 && 'Nothing here.'
         }
       </div>
     );
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import './TodoItem.css';
+import classNames from 'classnames';
 
 class TodoItem extends Component {
   render() {
     let { item } = this.props;
-    let className = 'TodoItem';
-    if (item.isComplete) {
-      className += ' TodoItem-complete';
-    }
+    
     return (
-      <div className={className}>
-        <p>{this.props.item.title}</p>
+      <div className={classNames('TodoItem', {
+        'TodoItem-complete': item.isComplete
+      })} onClick={this.props.onItemClicked}>
+        <p>{item.title}</p>
       </div>
     );
   }
