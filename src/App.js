e05ead5dc7ef9a077bd5e78170865a8d563f0041
Duy Phan <phuongduyphan@gmail.com>
2019-01-06 21:34:18 +0700

    finish basic function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import tick from './img/tick.svg';
 import './App.css';
 import TodoItem from './components/TodoItem';
 
@@ -7,6 +8,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
+      newItem: '',
       todoItems: [
         { title: 'Go Shopping' },
         { title: 'Play Football' },
@@ -31,14 +33,49 @@ class App extends Component {
     });
   }
 
+  onKeyUp = (event) => {
+    if (event.keyCode === 13) { // enter key
+      let text = event.target.value;
+      text = text.trim();
+      if (!text) {
+        return;
+      }
+
+      this.setState({
+        newItem: '',
+        todoItems: [
+          { title: text, isComplete: false },
+          ...this.state.todoItems
+        ]
+      })
+    }
+  }
+
+  onChange = (event) => {
+    let text = event.target.value;
+    this.setState({
+      newItem: text
+    })
+  }
+
   render() {
     return (
       <div className="App">
+        <div className="Header">
+          <img src={tick} width={32} height={32} />
+          <input
+            type="text"
+            placeholder="What needs to be done?"
+            value={this.state.newItem}
+            onChange={this.onChange}
+            onKeyUp={this.onKeyUp}
+          />
+        </div>
         {
-          this.state.todoItems.length > 0 && this.state.todoItems.map((item, index) => 
+          this.state.todoItems.length > 0 && this.state.todoItems.map((item, index) =>
             <TodoItem
-              key={index} 
-              item={item} 
+              key={index}
+              item={item}
               onItemClicked={this.onItemClicked(item)} />
           )
         }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import './TodoItem.css';
 import classNames from 'classnames';
+import checkImg from '../img/check.svg';
+import checkCompleteImg from '../img/check-complete.svg';
 
 class TodoItem extends Component {
   render() {
     let { item } = this.props;
-    
+    let url = checkImg;
+    if (item.isComplete) {
+      url = checkCompleteImg;
+    }
+
     return (
       <div className={classNames('TodoItem', {
         'TodoItem-complete': item.isComplete
-      })} onClick={this.props.onItemClicked}>
+      })}>
+        <img src={url} width={32} height={32} onClick={this.props.onItemClicked} />
         <p>{item.title}</p>
       </div>
     );
