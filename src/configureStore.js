3b44ddd25e24a20ccc43c4c357b82272d0f84f96
Duy Phan <phuongduyphan@gmail.com>
2019-02-01 00:43:46 +0700

    wrapping dispatch() to log actions

--- src/configureStore.js
+++ src/configureStore.js
@@ -3,6 +3,23 @@ import throttle from 'lodash/throttle';
 import rootReducer from './reducers'; // default index.js
 import { loadState, saveState } from './localStorage';
 
+const addLoggingToDispatch = (store) => {
+  const rawDispatch = store.dispatch;
+  if (!console.group) {
+    return rawDispatch;
+  }
+
+  return (action) => {
+    console.group(action.type);
+    console.log('%c prev state', 'color: gray', store.getState());
+    console.log('%c action', 'color: blue', action);
+    const returnValue = rawDispatch(action);
+    console.log('%c next state', 'color: green', store.getState());
+    console.groupEnd(action.type);
+    return returnValue;
+  }
+}
+
 const configureStore = () => {
   const persistedState = loadState();
   const store = createStore(
@@ -10,12 +27,16 @@ const configureStore = () => {
     persistedState
   );
 
+  if (process.env.NODE_ENV !== 'production') {
+    store.dispatch = addLoggingToDispatch(store);
+  }
+
   store.subscribe(throttle(() => {
     saveState({
       todos: store.getState().todos
     });
   }, 1000));
-  
+
   return store;
 }
 
